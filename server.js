af40bda1fbc425965982067e8165e479ab0cb194
Varoot Phasuthadol <varoot@gmail.com>
2017-02-12 23:18:39 +0800

    Set up for Heroku

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.post('/favorites', function(req, res){
   res.send(data);
 });
 
-app.listen(3000, function(){
-  console.log("Listening on port 3000");
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log("Listening on port " + server.address().port);
 });
